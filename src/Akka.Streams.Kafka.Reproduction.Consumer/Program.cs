153c19767c7a3766d885e9ae398406fef163f0cd
Aaron Stannard <aaron@petabridge.com>
2021-11-03 15:31:38 -0500

    added consumer program

--- src/Akka.Streams.Kafka.Reproduction.Consumer/Program.cs
+++ src/Akka.Streams.Kafka.Reproduction.Consumer/Program.cs
@@ -6,8 +6,10 @@ using Akka.Actor;
 using Akka.Event;
 using Akka.Streams.Dsl;
 using Akka.Streams.Kafka.Dsl;
+using Akka.Streams.Kafka.Helpers;
 using Akka.Streams.Kafka.Messages;
 using Akka.Streams.Kafka.Settings;
+using Akka.Util.Internal;
 using Confluent.Kafka;
 
 namespace Akka.Streams.Kafka.Reproduction
@@ -30,9 +32,44 @@ namespace Akka.Streams.Kafka.Reproduction
                 SocketKeepaliveEnable = true,
                 ConnectionsMaxIdleMs = 180000
             };
+
+            var consumerSettings = ConsumerSettings<Null, string>
+                .Create(actorSystem, null, null)
+                .WithBootstrapServers("localhost:29092")
+                .WithStopTimeout(TimeSpan.Zero)
+                .WithGroupId("group1");
             
+            var producerSettings = ProducerSettings<Null, string>.Create(actorSystem,
+                    null, null)
+                .WithBootstrapServers("localhost:29092");
+            
+            // TODO: we should just be able to accept a `ConsumerConfig` property
+            consumerConfig.ForEach(kv => consumerSettings = consumerSettings.WithProperty(kv.Key, kv.Value));
+
+            var committerSettings = CommitterSettings.Create(actorSystem);
+
+            var drainingControl = KafkaConsumer.CommittableSource(consumerSettings, Subscriptions.Topics("akka-input"))
+                .BackpressureAlert(LogLevel.WarningLevel, TimeSpan.FromMilliseconds(500))
+                .WithAttributes(Attributes.CreateName("CommitableSource"))
+                .Select(c => (c.Record.Topic, c.Record.Message.Value, c.CommitableOffset))
+                .SelectAsync(1, async t =>
+                {
+                    // simulate a request-response call that takes 10ms to complete here
+                    await Task.Delay(10);
+                    return t;
+                })
+                .Select(t => ProducerMessage.Single(new ProducerRecord<Null, string>($"{t.Topic}-done", t.Value),
+                    t.CommitableOffset))
+                .BackpressureAlert(LogLevel.WarningLevel, TimeSpan.FromMilliseconds(500))
+                .Via(KafkaProducer.FlexiFlow<Null, string, ICommittableOffset>(producerSettings)).WithAttributes(Attributes.CreateName("FlexiFlow"))
+                .Select(m => (ICommittable)m.PassThrough)
+                .Log("OutputLog").AddAttributes(Attributes.CreateLogLevels(LogLevel.InfoLevel))
+                .ToMaterialized(Committer.Sink(committerSettings), DrainingControl<NotUsed>.Create)
+                .Run(materializer);
+
+            Console.ReadLine();
+            await drainingControl.DrainAndShutdown();
+            await actorSystem.Terminate();
         }
-        
-        
     }
 }
